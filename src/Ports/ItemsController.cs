dce0a306d96630a0809ba8d7b2164f44cbc97868
Victor <victor@mail.ru>
2019-04-02 22:27:37 +0300

    aded project and base folders

---
+++ src/Ports/ItemsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ItemsService.Controllers
+{
+    
+    public class ItemsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Content("Hello world");
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Content("Posted!");
+        }
+    }
+}
---
+++ tests/ItemsControllerShould.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace ItemsService.Tests
+{
+    public class ItemsControllerShould
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
